cf1a72e8043bedf97142d3d7c4513d93e05e183d
Surya <surya@codingmart.com>
2019-05-14 11:33:21 +0530

    routess

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'fun#new'
 
-	match "/pricing" => redirect("/new")
+	get("/pricing") , to:'www.google.com'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
