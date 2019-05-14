a7249d1060c781e837b6a2337cd4a21e308b706a
Surya <surya@codingmart.com>
2019-05-14 10:43:30 +0530

    after route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 	root 'fun#new'
+
+	match "/pricing" => redirect("/new")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
