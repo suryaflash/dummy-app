73933b88b6bc2e832f22b8e80fdebe168779029b
Surya <surya@codingmart.com>
2019-05-14 12:11:36 +0530

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'fun#new'
 
-	get("/pricing") to: 'www.google.com';
+	get '/pricing' => redirect('https://www.google.com/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
