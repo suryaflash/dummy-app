81f1d5af61f73c5878271e2f56e02411c88d7c30
Surya <surya@codingmart.com>
2019-05-14 18:45:12 +0530

    a

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 	get '/pricing' => redirect('https://www.google.com/')
 	get '/blog' => redirect('https://www.facebook.com/')
 
+	
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
